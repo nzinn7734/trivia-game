43331c8898e474f3fe976ba020ef72f8414357a5
Noah <nzinn7734@gmail.com>
2025-03-30 11:02:22 -0400

    Added form for question search

--- src/app/question-search/question-search.component.ts
+++ src/app/question-search/question-search.component.ts
@@ -7,10 +7,13 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { CommonModule } from '@angular/common';
 import { QuestionCriteriaDataService } from '../services/question-criteria-data.service';
 import { QuestionCriteria } from '../models/question-criteria';
+import { FormControl, FormGroup } from '@angular/forms';
+import { QuestionSearchForm } from '../models/question-search-form';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-question-search',
-  imports: [RouterModule, MatProgressSpinnerModule, CommonModule],
+  imports: [RouterModule, MatProgressSpinnerModule, CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './question-search.component.html',
   styleUrl: './question-search.component.css'
 })
@@ -22,10 +25,49 @@ export class QuestionSearchComponent implements OnInit {
   ) {}
 
   categories$: Observable<TriviaCategoriesResponse> | undefined;
+  questionSearchForm: FormGroup<QuestionSearchForm>
   questionDate: QuestionCriteria | undefined
+  difficulties = [
+    {
+      value: '',
+      name: 'Any Difficulty'
+    },
+    {
+      value: 'easy',
+      name: 'Easy'
+    },
+    {
+      value: 'medium',
+      name: 'Medium'
+    },
+    {
+      value: 'hard',
+      name: 'Hard'
+    }
+  ]
+  type = [
+    {
+      value: '',
+      name: 'Any Type'
+    },  
+    {
+      value: 'multiple',
+      name: 'Multiple Choice'
+    },
+    {
+      value: 'boolean',
+      name: 'Ture/False'
+    }
+  ]
 
   ngOnInit(): void {
-    this.categories$ = this.questionService.getCategories()
+    this.categories$ = this.questionService.getCategories();
+    this.questionSearchForm = new FormGroup<QuestionSearchForm>({
+      amount: new FormControl('',  {nonNullable: true}),
+      category: new FormControl('', {nonNullable: true}),
+      difficulty: new FormControl('', {nonNullable: true}),
+      type: new FormControl('', {nonNullable: true})
+    });
   }
 
   public search() {
