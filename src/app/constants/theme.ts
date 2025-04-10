818a714475d29c2b26663ba6d8369ff1cd90ccf9
Noah <nzinn7734@gmail.com>
2025-04-10 19:58:46 -0400

    Added theme toggle component

---
+++ src/app/constants/theme.ts
@@ -0,0 +1,6 @@
+export const DARK_THEME_CORRECT = "#2c7f21";
+export const DARK_THEME_INCORRECT = "#822311";
+export const LIGHT_THEME_CORRECT = "lightgreen";
+export const LIGHT_THEME_INCORRECT = "lightcoral";
+export const LIGHT = "light";
+export const DARK = "dark";
---
+++ src/app/theme-toggle/theme-toggle.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { LIGHT_THEME_CORRECT, 
+  LIGHT_THEME_INCORRECT, 
+  DARK_THEME_CORRECT, 
+  DARK_THEME_INCORRECT,
+  LIGHT,
+  DARK
+} from '../constants/theme';
+
+@Component({
+  selector: 'app-theme-toggle',
+  imports: [],
+  templateUrl: './theme-toggle.component.html',
+  styleUrl: './theme-toggle.component.css'
+})
+export class ThemeToggleComponent {
+
+  private theme = document.getElementById("root")?.getAttribute("data-theme");
+
+  public themeToggle() {
+    let newTheme = this.theme === "dark" ? "light" : "dark"
+    this.theme = newTheme;
+    let correctAnswers = document.getElementsByClassName("outline correct");
+    let incorrectAnswers = document.getElementsByClassName("outline incorrect");
+    for(var i = 0; i < correctAnswers.length; i++) {
+      correctAnswers[i].setAttribute("style", "background-color:"+this.currentThemeCorrect())
+    }
+    for(var i = 0; i < incorrectAnswers.length; i++) {
+      incorrectAnswers[i].setAttribute("style", "background-color:"+this.currentThemeIncorrect())
+    }
+    document.getElementById("root")?.setAttribute("data-theme", newTheme);
+  }
+
+  private currentThemeCorrect() {
+    return this.theme === LIGHT ? LIGHT_THEME_CORRECT : DARK_THEME_CORRECT; 
+  }
+
+  private currentThemeIncorrect() {
+    return this.theme === LIGHT ? LIGHT_THEME_INCORRECT : DARK_THEME_INCORRECT;
+  }
+
+}
